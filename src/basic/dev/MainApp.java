48bc3f2d2f59c98946933f64c569482d6c98881c
dang diep <ddiep@gmail.com>
2022-08-23 18:42:46 +0700

    Project Syntax

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,33 @@
+package basic.dev;
+/*
+ *  Ghi chú ( comment nhiều dòng )
+ *  @author TranThanhBinh
+ *  1, Ten lop trùng tên file
+ *  2, Tên lớp chữ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu viết hoa
+ *  3, Tên lớp đặt phải có ý nghĩa.
+ *  - Interface: phải có tiền tố I
+ *  - Abstract: phải có tiền tố Abstract
+   */
+
+
+public class MainApp {
+	//Comment 1 dòng cách 1. Phím nóng Ctrl + /
+	/*Comment 1 dòng cách 2*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		/*
+		 * Cách đặt tên biến:
+		 * 1. Không chứa kí tự đặc biệt
+		 * 2. Không chứa khoảng trắng
+		 * 3. Viết thường
+		 * 4 .Không bắt đầu bằng số*/
+		int number;
+		String nameSchool;
+	}
+	/*
+	 * Tên phương thức tuân theo quy tắc camelCase
+	 * */
+	public void show() {
+		
+	}
+}
